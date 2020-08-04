a6e652216604511b97c7e73794e0512b87cf4bf0
tim <tim.veenendaal@gmail.com>
2020-08-04 23:41:11 +0200

    Add first controller and start on service

---
+++ src/Service/RunParserService.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Service;
+
+
+class RunParserService
+{
+    private $url = "https://www.uvponline.nl/uvponlineU/index.php/uvproot/wedstrijdschema/2020";
+
+    public function getRuns() {
+
+    }
+}
