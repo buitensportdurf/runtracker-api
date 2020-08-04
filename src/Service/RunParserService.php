24722f0230d3c3308776027bb8068918d7801760
tim <tim.veenendaal@gmail.com>
2020-08-05 00:55:06 +0200

    Make data parsing complete

--- src/Service/RunParserService.php
+++ src/Service/RunParserService.php
@@ -24,6 +24,13 @@ class RunParserService
             $data = [
                 'date' => $raw[0],
                 'city' => $raw[1],
+                'circuits' => [
+                    'L' => strlen($raw[2]) === 1,
+                    'M' => strlen($raw[3]) === 1,
+                    'K' => strlen($raw[4]) === 1,
+                    'J' => strlen($raw[5]) === 1,
+                ],
+                'qualifier' => $raw[6],
                 'distance' => $raw[7],
                 'age' => $raw[8],
                 'org' => [
