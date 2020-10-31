bd55e91627ae252911cbc9cda3ef3321c4429e7c
ardent <tim.veenendaal@gmail.com>
2020-10-31 18:37:53 +0100

    Add cors bundle for proper api cors

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
