152e5e0053f8ac2276e523a476c59b8c2801cae4
Tim <git@loken.nl>
2025-04-11 19:35:00 +0200

    Remove kpn paginator and translation

--- config/bundles.php
+++ config/bundles.php
@@ -13,6 +13,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
-    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
